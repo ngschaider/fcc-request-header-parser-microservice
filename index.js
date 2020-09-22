a0292a2fe1d2cc7728a95f00d0d8294026bd3925
ngschaider <gschaiderniklas@gmail.com>
2020-09-22 09:48:28 +0000

    added cors, fixed ip

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
+const cors = require("cors");
 const express = require("express");
 const app = express();
 
+
 const PORT = process.env.PORT || 3000;
+
+app.use(cors());
 
 app.get("/api/whoami", (req, res) => {
   const software = req.header("User-Agent");
-  const ipaddress = req.connection.ipaddress;
+  const ipaddress = req.ip;
   const language = req.header("Accept-Language");
   res.json({
     ipaddress,
